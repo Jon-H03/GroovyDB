72adf649a12318e53c60c53e587f704a84946f64
Jonathan Hirsch <112533132+Jon-H03@users.noreply.github.com>
2023-08-24 11:42:58 -0700

    Update delete_album.js

--- public/js/delete_album.js
+++ public/js/delete_album.js
@@ -1,8 +1,3 @@
-// Citation fr the following code:
-// Date: 08/09/2023
-// Copied from OSU nodejs-starter-app Github. Modified to run with our database
-// Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main/Step%207%20-%20Dynamically%20Deleting%20Data
-
 
 function deleteAlbum(albumID) {
     
