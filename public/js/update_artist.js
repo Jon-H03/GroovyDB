8c8b207a91fc9e20b5c5bc818b027fce45cad4de
Jonathan Hirsch <112533132+Jon-H03@users.noreply.github.com>
2023-08-24 11:41:55 -0700

    Update update_artist.js

--- public/js/update_artist.js
+++ public/js/update_artist.js
@@ -1,9 +1,3 @@
-// Citation for the following code:
-// Date: 08/02/2023
-// Copied and adapted from
-// OSU nodejs-starter-app Github. Modified to run with our database
-// Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main/Step%208%20-%20Dynamically%20Updating%20Data
-
 // Get the objects we need to modify
 let updateArtistForm = document.getElementById('update-artist-form-ajax');
 
